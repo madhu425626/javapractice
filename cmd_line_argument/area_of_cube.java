f0c57ded40952340cbc196835bd472beb3a2e5b7
madhu425626 <madhuabbugari@gmail.com>
2026-07-20 21:55:48 +0530

    calculating area of cube with user input . Using cmd line prompt

---
+++ cmd_line_argument/area_of_cube.java
@@ -0,0 +1,6 @@
+void main(String []args){
+	//int a =Integer.parseInt(args[0]);
+	//IO.println("Volume of the cube is: "+ args[0]);
+	IO.println("Volume of cube : "+ Math.pow(Double.parseDouble(args[0]),3));
+	
+}
