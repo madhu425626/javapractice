6a4a4c03a0bdb1b2d68520b27aaf70793e2b94dd
madhu425626 <madhuabbugari@gmail.com>
2026-08-15 21:05:47 +0530

    Create AreaCalculator.java

---
+++ BasicProblems/AreaCalculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        // Write your code here
+        double a = Double.parseDouble(IO.readln());
+        double b = Double.parseDouble(IO.readln());
+        
+        System.out.printf("Area of the room is: %.1f%n",a*b);
+    }
+}
