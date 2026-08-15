d93e77dcfa51bccb889880ce453494d1fdb4004c
madhu425626 <madhuabbugari@gmail.com>
2026-08-15 20:58:36 +0530

    Create AreaOfCube.java

---
+++ BasicProblems/AreaOfCube.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class AreaOfCubeMain {
+    public static void main(String[] args) {
+        // Write your code here
+        double vol = Double.parseDouble(IO.readln());
+
+        IO.println("Volume of the cube is: "+(vol*vol*vol)+" cubic cm");
+    }
+}
