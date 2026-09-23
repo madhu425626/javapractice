1fb0df62d5bbf5e76b60351909da5ec1ef4cfd4f
madhu425626 <madhuabbugari@gmail.com>
2026-09-23 08:25:10 +0530

    Add Prime Number Checker implementation

--- cmd_line_argument/prime_num.java
+++ cmd_line_argument/prime_num.java
@@ -19,3 +19,56 @@ boolean isPrime(int num){
 	return true;
 	
 }
+
+
+/*
+
+//Prime Number Revision
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      int input  = Integer.parseInt(IO.readln());
+      PrimeNumberChecker primeNumber = new PrimeNumberChecker();
+       boolean isPrimeOrNot =  primeNumber.isPrime(input);
+     IO.println("the given number is :"+input+" is Prime Number ? "+isPrimeOrNot);
+    }
+}
+
+class PrimeNumberChecker
+{
+  public boolean isPrime(int input)
+  {
+      if(input <=1)
+      {
+        //IO.println("is not priime");
+        return false;
+      }
+      else{
+        int count  = 0;
+
+        for(int i = 2; i<=input; i++){
+
+          if(input%i == 0)
+          {
+            count += 1;
+          }
+
+          
+          
+        }
+          if(count == 1)
+          {
+            //IO.println("is  priime");
+            return true;
+          }
+          else{
+            return false;
+           // IO.println("not a priime");
+          }
+           
+      }
+
+  }
+}
+
+*/
